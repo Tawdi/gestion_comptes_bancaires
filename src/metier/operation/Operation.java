e1edd0e98a5b2d098894f6bba3a4ce78f3c66fca
Tawdi <ahmedtawdi00@gmail.com>
2025-09-17 12:37:43 +0100

    feat: add abstract class Operation with attributes

---
+++ src/metier/operation/Operation.java
@@ -0,0 +1,24 @@
+package metier.operation;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public abstract class Operation {
+    protected UUID numero ;
+    protected LocalDateTime date;
+    protected double montant;
+
+    public Operation(double montant) {
+        this.numero = UUID.randomUUID();
+        this.date = LocalDateTime.now();
+        this.montant = montant;
+    }
+
+    public String toString() {
+        return "Operation{" +
+                "numero=" + numero +
+                ", date=" + date +
+                ", montant=" + montant +
+                '}';
+    }
+}
